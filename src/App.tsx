87a3fa07efcce4cbfa9549dbffc9f2f0ecc40f71
Stolbov Nikita <nikita-stolbov-99@mail.ru>
2025-04-14 20:13:25 +0300

    Consider the principle of operation of React Router (createBrowserRouter)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Menu/>
+  },
+  {
+    path: "/cart",
+    element: <Cart/>
+  },
+  {
+    path: "/",
+    element: <Error/>
+  }
+])
 
 function App() {
 
@@ -8,6 +27,11 @@ function App() {
       <Button>Стандартная кнопка</Button>
       <Button appearance='big'>Большая кнопка</Button>
       <Input type="email" placeholder='Email'/>
+      <div>
+        <a href="/">Menu</a><br />
+        <a href="/cart">Cart</a>
+      </div>
+      <RouterProvider router={router}></RouterProvider>
     </>
   );
 }
